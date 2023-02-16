f9f691049771dd60382f85e40496bbec4cc3b0a2
Madhusudan Sharma <sharmamadhusudan@hotmail.com>
2023-02-16 16:32:54 -0600

    Add project files.

---
+++ HRM.Infrastructure/Data/HRMDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRM.ApplicationCore.Entity;
+
+namespace HRM.Infrastructure.Data
+{
+    public class HRMDbContext:DbContext
+    {
+        public HRMDbContext(DbContextOptions<HRMDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<JobRequirement> JobRequirement { get; set; }
+        public DbSet<JobCategory> JobCategory { get; set; }
+        public DbSet<Candidate> Candidate { get; set; }
+        public DbSet<Submission> Submission { get; set; }
+    }
+}
